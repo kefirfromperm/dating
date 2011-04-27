706dcdbd2df65b1aae0e3e831c82624fb6eaca35
Vitalii Samolovskikh <kefir@perm.ru>
2011-04-28 00:34:00 +0600

    sitemap.xml

---
+++ dating/test/unit/com/greetim/SitemapControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.greetim
+
+import grails.test.ControllerUnitTestCase
+
+class SitemapControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
